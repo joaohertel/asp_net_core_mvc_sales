a4ae8866a383f78527114ca0c3cdb52ff6f83e76
unknown <joaohertels@gmail.com>
2023-07-08 23:11:33 -0300

    Integridade referencial

--- SalesMVC/Migrations/20230709020729_Adicionando Restrição de Chave Estrangeira em Seller via int.Designer.cs
+++ SalesMVC/Migrations/20230709020729_Adicionando Restrição de Chave Estrangeira em Seller via int.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesMVC.Data;
 
@@ -10,9 +11,11 @@ using SalesMVC.Data;
 namespace SalesMVC.Migrations
 {
     [DbContext(typeof(SalesMVCContext))]
-    partial class SalesMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20230709020729_Adicionando Restrição de Chave Estrangeira em Seller via int")]
+    partial class AdicionandoRestriçãodeChaveEstrangeiraemSellerviaint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
