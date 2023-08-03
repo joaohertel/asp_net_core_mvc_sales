19da54427ed0c021537d1e92e4b2e3926089f0b1
unknown <joaohertels@gmail.com>
2023-08-02 23:55:36 -0300

    Implementando busca simples

---
+++ SalesMVC/Services/SalesRecordsService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesMVC.Data;
+using SalesMVC.Models;
+
+namespace SalesMVC.Services
+{
+    public class SalesRecordsService
+    {
+        // find by date
+        // adicionar o contexto
+
+        private SalesMVCContext _context;
+
+        public SalesRecordsService(SalesMVCContext context) {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (!minDate.HasValue)
+            {
+                // primeiro de janeiro do ano atual
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            result = result.Where(obj => obj.Date >= minDate && obj.Date <= maxDate);
+
+            return await result
+                .Include(obj => obj.Seller)
+                .Include(obj => obj.Seller.Department)
+                .OrderByDescending(obj => obj.Date)
+                .ToListAsync();
+            // join com Department, Seller e ordenado por ordem decrescente
+        }
+
+    }
+}
